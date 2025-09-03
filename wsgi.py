983e4f1f9672975522d8810f9e799a22c2b7d1ca
ALejandro C <Alejo0789@hotmail.com>
2025-09-03 12:36:18 -0500

    saving telemetry corrected

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,6 @@
 # wsgi.py
-import os
-import sys
-
-# Add the current directory to the Python path
-# This is usually not needed on Railway, but it's good practice
-# for local testing.
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-# Import the app instance directly from your app.py file
 from app import create_app
 
-# The Gunicorn callable. It must be named 'app'
-# as per your Procfile, which is `wsgi:app`.
+# Gunicorn needs a callable named 'app'
+# It will use this to start your Flask application
 app = create_app()
